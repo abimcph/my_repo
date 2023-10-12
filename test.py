f703ceff03148f4854fa9ec988c7e6a0c26d32c1
abimcph <abimcph@yahoo.com>
2023-10-12 13:16:35 +0100

    Changed script for addition

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 # test.py
 var1 = 5
 var2 = 3
-result = var1 * var2
-print(f"The result of multiplying {var1} and {var2} is {result}
+result = var1 + var2
+print(f"The result of adding {var1} and {var2} is {result}
